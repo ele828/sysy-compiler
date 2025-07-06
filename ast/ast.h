917d453760bbb95e71375330a656c28f0f868cbc
ele828 <ele828@gmail.com>
2025-07-06 23:38:01 +0800

    [parser] init AST

--- ast/ast.h
+++ ast/ast.h
@@ -1,4 +1,62 @@
 #pragma once
 
+#include "base/zone.h"
+#include "base/zone_container.h"
+
 namespace sysy {
-}
+
+class Decl;
+
+class AstNode : public ZoneObject {
+ public:
+  enum class Kind : uint8_t {
+    kCompilationUnit,
+
+    // Declaration start
+    kConstDecl,
+    kVarDecl,
+    kFunDecl,
+    // Declaration end
+  };
+
+  explicit AstNode(Kind kind) : kind_(kind) {}
+
+  Kind kind() const { return kind_; }
+
+ private:
+  Kind kind_;
+};
+
+class CompilationUnit : public AstNode {
+ public:
+  static bool classof(const AstNode& n) {
+    return n.kind() == Kind::kCompilationUnit;
+  }
+
+ private:
+  ZoneVector<Decl*> body_;
+};
+
+class Decl : public AstNode {
+ public:
+  static bool classof(const AstNode& n) {
+    return n.kind() >= Kind::kConstDecl && n.kind() <= Kind::kFunDecl;
+  }
+};
+
+class ConstDecl : public Decl {
+ public:
+  static bool classof(const AstNode& n) { return n.kind() == Kind::kConstDecl; }
+};
+
+class VarDecl : public Decl {
+ public:
+  static bool classof(const AstNode& n) { return n.kind() == Kind::kVarDecl; }
+};
+
+class FunDecl : public Decl {
+ public:
+  static bool classof(const AstNode& n) { return n.kind() == Kind::kFunDecl; }
+};
+
+}  // namespace sysy
