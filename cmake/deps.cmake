9a7f55f75ec896f86fdf130b90b2f223ffbb43da
ele828 <ele828@gmail.com>
2025-07-20 00:03:49 +0800

    [cmake] use add_dependency method to add third_party deps

--- cmake/deps.cmake
+++ cmake/deps.cmake
@@ -1,19 +1,23 @@
-function(fetch_dependencies)
+function (add_dependency name url tag)
   include(FetchContent OPTIONAL)
   FetchContent_Declare(
-    fast_float
-    GIT_REPOSITORY https://github.com/fastfloat/fast_float.git
-    GIT_TAG tags/v8.0.2
+    ${name}
+    GIT_REPOSITORY ${url}
+    GIT_TAG ${tag}
     DOWNLOAD_EXTRACT_TIMESTAMP FALSE
+    GIT_PROGRESS TRUE
     GIT_SHALLOW TRUE)
+  FetchContent_MakeAvailable(${name})
+endfunction()
 
-  FetchContent_Declare(
-    googletest
-    GIT_REPOSITORY https://github.com/google/googletest.git
-    GIT_TAG v1.17.0
-    DOWNLOAD_EXTRACT_TIMESTAMP FALSE
-    GIT_SHALLOW TRUE)
-  FetchContent_MakeAvailable(
+function(fetch_dependencies)
+  add_dependency(
     fast_float
-    googletest)
+    https://github.com/fastfloat/fast_float.git
+    tags/v8.0.2)
+
+  add_dependency(
+    googletest
+    https://github.com/google/googletest.git
+    v1.17.0)
 endfunction()
