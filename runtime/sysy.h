09e55c4c465a7baf8ab624a6569257fe99896d10
ele828 <ele828@gmail.com>
2026-06-21 22:31:29 +0800

    Runtime: add sysy runtime library

---
+++ runtime/sysy.h
@@ -0,0 +1,24 @@
+#pragma once
+
+// Reads an integer from the standard input.
+int getint();
+
+// Reads a character from the standard input.
+int getch();
+
+// Reads an array of integers from the standard input.
+int getarray(int a[]);
+
+// Writes an integer to the standard output.
+void putint(int num);
+
+// Writes a character to the standard output.
+void putch(int ch);
+
+// Writes an array of integers to the standard output.
+void putarray(int n, int a[]);
+
+// Starts and stops the timer.
+// Nested calls to these functions are not allowed.
+void starttime();
+void stoptime();
