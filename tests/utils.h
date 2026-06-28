52a3388fdc33ca79c6e41dd126479205da32df29
ele828 <ele828@gmail.com>
2026-06-28 23:26:18 +0800

    Test: rename sysy extension from .sy to .c

--- tests/utils.h
+++ tests/utils.h
@@ -35,7 +35,7 @@ struct Fixture {
 inline std::vector<Fixture> DiscoverFixtures(std::filesystem::path path) {
   std::vector<Fixture> fixtures;
   for (const auto& entry : fs::recursive_directory_iterator(path)) {
-    if (fs::is_regular_file(entry) && entry.path().extension() == ".sy") {
+    if (fs::is_regular_file(entry) && entry.path().extension() == ".c") {
       fixtures.emplace_back(entry.path().filename().string(), entry.path());
     }
   }
