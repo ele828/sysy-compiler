65745d0634607efb73fbe7132fcdcded1f274b5b
ele828 <ele828@gmail.com>
2026-07-12 23:06:17 +0800

    Base: add linked list

--- base/logging.h
+++ base/logging.h
@@ -1,10 +1,29 @@
 #pragma once
 
-#include <cassert>
+#include <print>
 
 namespace {
 
-#define NOTREACHED() assert(false)
-#define DCHECK(cond) assert(cond)
+#define CHECK(condition)                                                      \
+  do {                                                                        \
+    if (!(condition)) {                                                       \
+      std::println(stderr, "Check failed: {} at {}:{}", #condition, __FILE__, \
+                   __LINE__);                                                 \
+      std::abort();                                                           \
+    }                                                                         \
+  } while (0)
+
+#if !defined(NDEBUG)
+#define DCHECK(cond) CHECK(cond)
+#else
+#define DCHECK(condition)        \
+  do {                           \
+    if (false) {                 \
+      bool unused = (condition); \
+      (void)unused;              \
+    }                            \
+  } while (0)
+#endif
+#define NOTREACHED() DCHECK(false)
 
 }  // namespace
